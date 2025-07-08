be4e24646d9d211f0c1a57550fa741b0309327fd
Jasen Peterson <me@jasenmp.com>
2025-07-08 08:28:59 -0500

    Implementing product-title.tsx

--- components/product-form/create-product-form.tsx
+++ components/product-form/create-product-form.tsx
@@ -1,9 +1,17 @@
 import { Form } from "@heroui/form";
+import ProductTitle from "@/components/product-form/product-title";
 
 export default function CreateProductForm() {
   return (
-    <Form className="w-full max-w-4xl mx-auto p-4 bg-white shadow-md rounded-lg">
-      <h2 className="text-black">Create Product</h2>
-    </Form>
+    <section className="w-full max-w-4xl mx-auto p-4 bg-white shadow-md rounded-lg">
+      <div>
+        <h2 className="text-base/7 font-semibold text-gray-900">
+          Create Product
+        </h2>
+      </div>
+      <Form>
+        <ProductTitle />
+      </Form>
+    </section>
   );
 }
---
+++ components/product-form/product-title.tsx
@@ -0,0 +1,9 @@
+import { Input } from "@heroui/input";
+
+export default function ProductTitle() {
+  return (
+    <div className="mt-2 w-full">
+      <Input className="w-full" label="Product Title" type="text" />
+    </div>
+  );
+}
