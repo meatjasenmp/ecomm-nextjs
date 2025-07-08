4a05e15548931759225b56f3c99ea922dcd5cef7
Jasen Peterson <me@jasenmp.com>
2025-07-08 08:08:58 -0500

    Humble beginnings

---
+++ app/admin/products/create/page.tsx
@@ -0,0 +1,5 @@
+import CreateProductForm from "@/components/product-form/create-product-form";
+
+export default async function Page() {
+  return <CreateProductForm />;
+}
---
+++ components/product-form/create-product-form.tsx
@@ -0,0 +1,9 @@
+import { Form } from "@heroui/form";
+
+export default function CreateProductForm() {
+  return (
+    <Form className="w-full max-w-4xl mx-auto p-4 bg-white shadow-md rounded-lg">
+      <h2 className="text-black">Create Product</h2>
+    </Form>
+  );
+}
