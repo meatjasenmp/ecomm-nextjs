43b048373976016e19c4f54c767f2645ad8cdbf3
Jasen Peterson <me@jasenmp.com>
2025-07-09 08:31:32 -0500

    Implementing validation for product-description.tsx.

--- components/product-form/product-description.tsx
+++ components/product-form/product-description.tsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import { Textarea } from "@heroui/input";
 import InputContainer from "@/components/product-form/input-container";
-import { InputValidation } from "@/components/product-form/types";
+import ErrorDisplay from "@/components/product-form/error-display";
 
-export default function ProductDescription({ isInvalid }: InputValidation) {
+export default function ProductDescription() {
+  const [value, setValue] = useState<string>("");
+  const errors: string[] = [];
+  if (value.length > 0 && value.length < 10) {
+    errors.push("The Product Description must be at least 10 characters long.");
+  }
+
   return (
     <InputContainer>
       <Textarea
         name="description"
         label="Description"
         variant="underlined"
-        isInvalid={isInvalid}
+        errorMessage={() => <ErrorDisplay errors={errors} />}
+        isRequired
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        isInvalid={errors.length > 0}
       />
     </InputContainer>
   );
