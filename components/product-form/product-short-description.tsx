f659aa5ba8820af4d0e9c99c6f3cebba79f842ca
Jasen Peterson <me@jasenmp.com>
2025-07-09 08:39:30 -0500

    Making this controlled.

--- components/product-form/product-short-description.tsx
+++ components/product-form/product-short-description.tsx
@@ -1,7 +1,10 @@
 import { Textarea } from "@heroui/input";
 import InputContainer from "@/components/product-form/input-container";
+import { useState } from "react";
 
 export default function ProductShortDescription() {
+  const [value, setValue] = useState<string>("");
+
   return (
     <InputContainer>
       <Textarea
@@ -9,6 +12,8 @@ export default function ProductShortDescription() {
         label="Short Description"
         variant="underlined"
         description="Optional short description of the product."
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
       />
     </InputContainer>
   );
