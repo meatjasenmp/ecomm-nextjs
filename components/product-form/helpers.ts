e085a48feda6598f2c25dd3f5ef5724d8b3cd4ac
Jasen Peterson <me@jasenmp.com>
2025-07-09 08:19:32 -0500

    Nuking this (for now).

--- components/product-form/helpers.ts
+++
@@ -1,8 +0,0 @@
-import { ErrorProperties } from "@/actions/product-form/types";
-
-export function validateInput(
-  errors: ErrorProperties | undefined,
-  fieldName: string,
-): boolean {
-  return errors ? Object.keys(errors).includes(fieldName) : false;
-}
