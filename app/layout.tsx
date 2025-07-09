da7f2ab8b896231bd155e0816f55376df91794dd
Jasen Peterson <me@jasenmp.com>
2025-07-09 18:43:19 -0500

    Importing React.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import "@/styles/globals.css";
 import { Metadata, Viewport } from "next";
 import clsx from "clsx";
-
+import React from "react";
 import { Providers } from "./providers";
-
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 
