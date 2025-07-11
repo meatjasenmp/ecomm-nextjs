fdd975cf3a68c03f403b7664c38247dac88707cb
Jasen Peterson <me@jasenmp.com>
2025-07-11 07:32:20 -0500

    Updating validation for product-description.tsx

--- components/product-form/product-description.tsx
+++ components/product-form/product-description.tsx
@@ -1,14 +1,13 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { Textarea } from "@heroui/react";
 import InputContainer from "@/components/product-form/input-container";
 import ErrorDisplay from "@/components/product-form/error-display";
 
 export default function ProductDescription() {
   const [value, setValue] = useState<string>("");
-  const errors: string[] = [];
-  if (value.length > 0 && value.length < 10) {
-    errors.push("The Product Description must be at least 10 characters long.");
-  }
+  let error: string = "";
+  const isValid = useMemo(() => value.length > 0 && value.length < 5, [value]);
+  if (isValid) error = "Title must be at least 5 characters long.";
 
   return (
     <InputContainer>
@@ -16,12 +15,12 @@ export default function ProductDescription() {
         name="description"
         label="Description"
         variant="underlined"
-        errorMessage={() => <ErrorDisplay errors={errors} />}
+        errorMessage={() => <ErrorDisplay error={error} />}
         description="Enter a concise description of the product."
         isRequired
         value={value}
         onChange={(e) => setValue(e.target.value)}
-        isInvalid={errors.length > 0}
+        isInvalid={error.length > 0}
       />
     </InputContainer>
   );
