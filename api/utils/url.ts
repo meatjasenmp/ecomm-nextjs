12f7447f9538647cb93f377c026ae331ee05c1ca
Jasen Peterson <me@jasenmp.com>
2025-07-15 14:50:16 -0500

    Removing unused files

--- api/utils/url.ts
+++
@@ -1,5 +0,0 @@
-export const getApiUrl = (endpoint: string) => {
-  return typeof window === "undefined"
-    ? `${process.env.API_URL || "http://localhost:8080"}${endpoint}`
-    : `/api${endpoint}`;
-};
