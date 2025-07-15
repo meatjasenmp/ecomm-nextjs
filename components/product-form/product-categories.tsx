d75b3ea979aa9d9821fe6be7f7eec1f321832a54
Jasen Peterson <me@jasenmp.com>
2025-07-15 09:02:08 -0500

    Converting this to a controlled component.

--- components/product-form/product-categories.tsx
+++ components/product-form/product-categories.tsx
@@ -1,39 +1,63 @@
 "use client";
 
-import { useMemo, useState } from "react";
 import { Selection, Select, SelectItem } from "@heroui/react";
+import { useFormContext, Controller } from "react-hook-form";
 
 import InputContainer from "@/components/product-form/input-container";
 import { CategoriesProps } from "@/app/admin/products/create/page";
+import ErrorDisplay from "@/components/product-form/error-display";
+import { Category } from "@/api/categories/types";
 
-// TODO: Only getting the keys here, will have to convert to the full category object in api requests
 export default function ProductCategories({ categories }: CategoriesProps) {
-  const [selected, setSelected] = useState<Selection>(new Set([]));
-  const [touched, setTouched] = useState<boolean>(false);
-  // @ts-ignore
-  const isValid = useMemo(() => selected.size > 0, [selected]);
+  const {
+    control,
+    formState: { errors },
+  } = useFormContext();
+  const error = errors.categories?.message as string;
 
   return (
     <InputContainer>
-      <Select
-        isRequired
-        description="Select one or more categories for the product."
-        errorMessage={
-          isValid || !touched ? undefined : "Select at least one category."
-        }
-        id="product-categories"
-        isInvalid={!(isValid || !touched)}
-        placeholder="Select categories"
-        selectedKeys={selected}
-        selectionMode="multiple"
-        variant="underlined"
-        onClose={() => setTouched(true)}
-        onSelectionChange={setSelected}
-      >
-        {categories.map((cat) => (
-          <SelectItem key={cat._id}>{cat.name}</SelectItem>
-        ))}
-      </Select>
+      <Controller
+        control={control}
+        name="categories"
+        render={({ field: { onChange, value } }) => {
+          const selectedKeys = new Set<string>(
+            value?.map((cat: Category) => cat._id) || [],
+          );
+
+          const handleSelectionChange = (keys: Selection) => {
+            const selectedCategories = categories.filter((cat) =>
+              Array.from(keys).includes(cat._id),
+            );
+            onChange(selectedCategories);
+          };
+
+          return (
+            <Select
+              isRequired
+              description="Select one or more categories for the product."
+              errorMessage={() => <ErrorDisplay error={error || ""} />}
+              id="product-categories"
+              isInvalid={!!error}
+              placeholder="Select categories"
+              selectedKeys={selectedKeys}
+              selectionMode="multiple"
+              variant="underlined"
+              onSelectionChange={handleSelectionChange}
+            >
+              {categories.map((cat) => (
+                <SelectItem key={cat._id}>{cat.name}</SelectItem>
+              ))}
+            </Select>
+          );
+        }}
+        rules={{
+          required: "At least one category is required",
+          validate: (value) => {
+            return value || value.length !== 0;
+          },
+        }}
+      />
     </InputContainer>
   );
 }
