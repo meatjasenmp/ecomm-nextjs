bc7341237ecc2b63d44e48286b9dcd06e65d40ba
Jasen Peterson <me@jasenmp.com>
2025-07-15 12:58:52 -0500

    Cleaning this up

--- api/products/types.ts
+++ api/products/types.ts
@@ -3,7 +3,7 @@ import { z } from "zod/v4";
 import { CategorySchema } from "../categories/types";
 import { ImageSchema } from "../images/types";
 
-const BaseProductSchema = z.object({
+export const ProductSchema = z.object({
   _id: z.string().optional(),
   title: z.string().min(5, "Title must be at least 5 characters long"),
   description: z
@@ -16,18 +16,12 @@ const BaseProductSchema = z.object({
   categories: z
     .array(CategorySchema)
     .min(1, "At least one category is required"),
+  images: z
+    .union([z.array(z.instanceof(File)), z.array(ImageSchema)])
+    .refine((images) => images.length > 0, "At least one image is required"),
   price: z.number().min(1, "Price must be greater than 0"),
   discount: z.number().min(0, "Discount cannot be negative").optional(),
   isPublished: z.boolean().default(true),
 });
 
-export const ProductCreateSchema = BaseProductSchema.extend({
-  images: z.array(z.instanceof(File)).min(1, "At least one image is required"),
-});
-
-export const ProductSchema = BaseProductSchema.extend({
-  images: z.array(ImageSchema).min(1, "At least one image is required"),
-});
-
 export type Product = z.infer<typeof ProductSchema>;
-export type ProductCreate = z.infer<typeof ProductCreateSchema>;
